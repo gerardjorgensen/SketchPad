159cca63daa9ddfb22c2a1cf9c3e065b2e620e80
gerardjorgensen <gerard.jorgensen@gmail.com>
2015-06-25 00:47:34 -0700

    js and cs files added

---
+++ SketchPad.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$("body").append("<div class='container'></div>");
+
+	$('.container').append("<div class='row'></div>")
+	.append("<div class='row'></div>")
+	.append("<div class='row'></div>")
+	.append("<div class='row'></div>")
+
+	$('.container').append("<div class='column'></div>")
+	.append("<div class='column'></div>")
+	.append("<div class='column'></div>")
+	.append("<div class='column'></div>")
+
+});
